f3b4343debc2bb9b71fb0df0908d429bb645cd73
Khaled Alkiek <k.alkiek@gmail.com>
2020-02-05 02:32:58 +0200

    Update abstract stitcher

--- synopsis/stitching/abstract_stitcher.py
+++ synopsis/stitching/abstract_stitcher.py
@@ -1,4 +1,6 @@
-from typing import List
+import numpy as np
+from typing import List, NoReturn
+from nptyping import Array
 from abc import ABC, abstractmethod
 
 from object.activity.activity_tube import ActivityTube
@@ -8,5 +10,13 @@ class AbstractStitcher(ABC):
     # TODO: add return type hints for methods
 
     @abstractmethod
-    def stitch(self, activity_tubes: List[ActivityTube], schedule: List[int]):
+    def initialize(self, activity_tubes: List[ActivityTube], schedule: List[int]) -> NoReturn:
+        pass
+
+    @abstractmethod
+    def has_next_frame(self) -> bool:
+        pass
+
+    @abstractmethod
+    def next_frame(self) -> Array[np.int]:
         pass
