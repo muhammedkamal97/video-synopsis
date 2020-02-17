5c3eee19d18ca1e7efb2719c7886acd0dcb66e5b
Muhammed Kamal <muhammedkamal97@githup.com>
2020-02-17 22:57:30 +0200

    caching video detections

---
+++ object/detection/cach_detector.py
@@ -0,0 +1,21 @@
+import cv2
+import json
+import os
+from object.detection.abstract_object_detector import *
+
+class CachDetector(AbstractObjectDetector):
+    
+    def __init__(self,arg):
+        super(CachDetector, self).__init__()
+        with open(os.path.sep.join(['cach_detector', arg['video_name']])) as json_file:
+            self.detections = json.load(json_file)
+        self.frame_num = 1
+        
+    def detect(self, frame: Array[np.int]) -> List[BoundingBox]:
+        boxes = []
+        if str(self.frame_num) not in self.detections:
+            return None 
+        for box in self.detections[str(self.frame_num)]:
+            boxes.append(BoundingBox(box['upper_left'],box['lower_right']))
+        self.frame_num += 1
+        return boxes
