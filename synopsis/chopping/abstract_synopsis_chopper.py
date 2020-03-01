d2bb37c3361a8aad596798b27f0c0eb5fd70e8e4
Khaled Alkiek <k.alkiek@gmail.com>
2020-03-01 13:21:36 +0200

    Implement master module

--- synopsis/chopping/abstract_synopsis_chopper.py
+++ synopsis/chopping/abstract_synopsis_chopper.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 
+from object.activity.activity_aggreagator import ActivityAggregator
+
 
 class AbstractSynopsisChopper(ABC):
     # TODO: add return type hints for methods
 
     @abstractmethod
-    def to_chop(self, activity_tubes) -> bool:
+    def to_chop(self, activity_aggregator: ActivityAggregator) -> bool:
         pass
